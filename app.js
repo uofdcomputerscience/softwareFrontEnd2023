5d04637287adcd350a16e6777b07fba633fe7a85
Teresa Reyna <treyna@udallas.edu>
2023-04-13 00:02:36 -0500

    login format

--- app.js
+++ app.js
@@ -1,13 +1,82 @@
-const app = Vue.createApp({
-    data() {        
+let app = Vue.createApp({
+    data: function() {        
         return {
             num: 1
+        }
+    },
+    methods: {
+        toggleList(){
+            this.num = 13
+        }, 
+        toggleCalendar(){
+            Calendar 
         }
     }
 })
 
-app.mount('#app')
+    app.component('login-form', {
+        template: `
+            <form @submit="handleSubmit">
+                <h1>{{ title }}</h1>
+                <p v-for="str in inputs">{{ str }}</p>
+                <custom-input v-model="email":label="emailLabel" />
+                <custom-input v-model="password":label="passwordLabel"/>
+                <button>Log in</button>
+            </form>`
+        ,
+        components: ['custom-input'],
+        data() {
+            return {
+                title: "Login Form",
+                inputs: [
+                    'email',
+                    'password',
+                    'name'
+                ],
+                email: '', //props not mutated, these are updated 
+                password: '',
+                emailLabel: 'Email',
+                passwordLabel: 'Password'
 
+            }
+        },
+        methods: {
+            handleSubmit(){
+                console.log(this.email, this.password)
+            }
+        }
+    })
+
+    app.component('custom-input',{
+        template: `
+            <label>
+                {{ label }}
+                <input type="text" v-model="inputValue">
+            </label>
+        `,
+        props: ['label', 'modelValue'],
+        computed: {
+            inputValue: {
+                get() {
+                    return this.modelValue            
+                },
+                set(value){//modelling variable from child to parent
+                    console.log(value)
+                    this.$emit('update:modelValue', value)//emiting event to parent, parent is listening to the event 
+                }
+            }
+
+        }
+       /* data(){
+            return{
+                inputValue: ''
+            }
+        }
+        */
+
+    })
+
+app.mount('#app')
 
 
 
